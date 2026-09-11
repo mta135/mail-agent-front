39aeb8ba5d60770402cc28442c93c0f8ec3b16ba
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2026-09-11 14:02:12 +0300

    mail details

--- src/app/pages/inbox/inbox.component.ts
+++ src/app/pages/inbox/inbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { MailService } from '../../services/mail.service';
 import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inbox',
@@ -11,11 +12,14 @@ import { DatePipe } from '@angular/common';
 export class InboxComponent {
 
   mailService = inject(MailService);
-
+  private router = inject(Router);
 
   inboxMails = computed(() =>
     this.mailService.mails().filter(mail => mail.folderId === 'inbox')
   );
 
+  openMail(id: string): void {
+    this.router.navigate(['/mail', id]);
+  }
 
 }
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,9 +1,10 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { InboxComponent } from '../pages/inbox/inbox.component';
+import { MailDetailComponent } from '../components/mail-detail/mail-detail.component';
 
 export const routes: Routes = [
-
     { path: '', redirectTo: 'inbox', pathMatch: 'full' },
     { path: 'inbox', component: InboxComponent },
+    { path: 'mail/:id', component: MailDetailComponent },
 ];
